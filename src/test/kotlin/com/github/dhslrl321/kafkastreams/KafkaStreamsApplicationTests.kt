33aea535f09fe88189dbb79fd4ef488eca2a28ce
jangwonik <dhslrl321@gmail.com>
2025-02-11 01:15:27 +0900

    commit

--- src/test/kotlin/com/github/dhslrl321/kafkastreams/KafkaStreamsApplicationTests.kt
+++ src/test/kotlin/com/github/dhslrl321/kafkastreams/KafkaStreamsApplicationTests.kt
@@ -1,13 +1,32 @@
 package com.github.dhslrl321.kafkastreams
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+
 
 @SpringBootTest
+@EmbeddedKafka(
+    partitions = 1,
+    brokerProperties = ["listeners=PLAINTEXT://localhost:9092"]
+)
 class KafkaStreamsApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    private val props: Map<String, Any> = java.util.Map.of<String, Any>(
+        "bootstrap.servers", "localhost:9092",
+        "key.serializer", "org.apache.kafka.common.serialization.StringSerializer",
+        "value.serializer", "org.apache.kafka.common.serialization.StringSerializer"
+    )
 
+    var producer: KafkaProducer<String, String> = KafkaProducer(props)
+
+    @Test
+    fun sample() {
+        // message produce to broker
+        val record = ProducerRecord<String, String>("a-topic", "hi~")
+
+        producer.send(record)
+    }
 }
